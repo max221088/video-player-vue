0231859b702e93a9b317ce731ef8dd25b038e815
max221088 <117590988+max221088@users.noreply.github.com>
2023-08-19 20:22:05 +0300

    first innit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="less">
+@import './assets/style/index.less'
+</style>
