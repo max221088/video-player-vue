2df8503e0ea0c5243fe2f438224e0525d46c60c4
max221088 <117590988+max221088@users.noreply.github.com>
2023-08-22 01:07:04 +0300

    add control panel show params

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,33 +1,47 @@
 <template>
-  <div class="wropper">
+  <div class="wropper" :style="{width: videoWidth}">
     <video 
-    ref="player" width="720" height="400"
+    ref="player" width="720" 
+    @mousemove="onMoveMause()"
+    @click="playTogle()"
     @timeupdate="updateCurrentTime()" 
     @canplaythrough="getInnitParams()"
     >
-  <source src="../assets/video/example-video.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>
-<div>
-  <div class="scroll-bar" ref="scrollBar"
-  :style="{width: videoWidth}"
-  @click="seek($event)">
-    <div class="seek" :style="{width: seekWidth}"></div>
-  </div>
-  {{ getCurrentTime }} / {{ getTotalTime }} 
-  <button @click="play()">Play/Pause</button>
-  <button @click="loop()">Loop</button>
-  <button @click="mute()">Mute</button>
-  <span>Speed: </span>
-  <select v-model="speed" @change="speedChange()">
-    <option value="1">1</option>
-    <option value="1.25">1.25</option>
-    <option value="1.5">1.5</option>
-    <option value="1.75">1.75</option>
-    <option value="2">2</option>
-  </select>
-  <input type="range" v-model="volume" @change="changeVolume()">
-</div>
+    <source src="../assets/video/example-video.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+  </video>
+  <div class="control-panel" 
+    v-if="totalTime != 0"
+    @mouseenter="showControlPanel()"
+    @mouseleave="hideControlPanel($event)"
+    v-bind:class="{active: isPanelShow}">
+    <div class="scroll-bar" ref="scrollBar"
+      :style="{width: videoWidth}"
+      @click="seek($event)">
+      <div class="seek" :style="{width: seekWidth}"></div>
+    </div>
+    {{ getCurrentTime }} / {{ getTotalTime }} 
+    <button @click="playTogle()">Play/Pause</button>
+    <button @click="loop()">Loop</button>
+    <button @click="mute()">Mute</button>
+    <span>Speed: </span>
+    <select v-model="speed" @change="speedChange()">
+      <option value="1">1</option>
+      <option value="1.25">1.25</option>
+      <option value="1.5">1.5</option>
+      <option value="1.75">1.75</option>
+      <option value="2">2</option>
+    </select>
+    <input type="range" v-model="volume" @change="changeVolume()">
+    </div>
+    <div class="alert-play" v-bind:class="{active: isShowPlay}">
+      <img src="../assets/icon/play_icon.svg">
+    </div>
+    <div class="alert-play"
+      @click="playTogle()"
+      v-bind:class="{active: isShowPause}">
+      <img src="../assets/icon/pause_icon.svg">
+    </div>
   </div>
 </template>
 
@@ -45,10 +59,28 @@ export default {
       videoWidth: '0px',
       speed: '1',
       volume: '50',
-      isPlay: false
+      isPlay: false,
+      isShowPlay: false,
+      isShowPause: false,
+      isPanelShow: true,
+      interval: setTimeout(() => {this.isPanelShow = false}, 3000),
     }
   },
   methods:{
+    onMoveMause() {
+      clearTimeout(this.interval)
+      this.isPanelShow = true;
+      this.interval = setTimeout(() => {this.isPanelShow = false}, 3000)
+    },
+    showControlPanel() {
+      clearTimeout(this.interval)
+      this.isPanelShow = true;
+    },
+    hideControlPanel (event) {
+      if (event.fromElement.attributes[0].nodeValue.toString() != 'control-panel') {
+        setTimeout(() => {this.isPanelShow = false}, 3000);
+      }
+    },
     changeVolume() {
       this.$refs.player.volume = this.volume/100
       console.log(this.volume/100)
@@ -84,9 +116,21 @@ export default {
       this.currentTime = this.$refs.player.currentTime
       this.seekWidth = (this.$refs.player.currentTime * 100 / this.$refs.player.duration) + '%';
     },
-    play () {
+    playTogle () {
       this.isPlay = !this.isPlay;
-      this.isPlay ? this.$refs.player.play() : this.$refs.player.pause()
+      this.isPlay ? this.play() : this.pause()
+    },
+    pause () {
+      this.isShowPause = true;
+      this.$refs.player.pause()
+    },
+    play () {
+      this.isShowPause = false;
+      this.isShowPlay = true;
+      this.$refs.player.play()
+      setTimeout(() => {
+        this.isShowPlay = false;
+      }, 500);
     }
   },
   computed: {
